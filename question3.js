eeda5313ff679fc73f875d30f24dc25cac768f96
Rameezkhan4563 <160046785+Rameezkhan4563@users.noreply.github.com>
2024-06-05 21:50:26 +0500

    changing in question 3

--- question3.js
+++ question3.js
@@ -1,5 +1,13 @@
 //Name Cases: Store a person’s name in a variable, and then print that person’s name in lowercase, uppercase, and titlecase.
-var personName1 = "rameez ahmed";
+var personName1 = "My name is Rameez ahmed khan";
 console.log(personName1.toLowerCase());
 console.log(personName1.toUpperCase());
-console.log(personName1.charAt(0).toUpperCase() + personName1.slice(1).toLowerCase());
+//step 1:
+var words = personName1.split(" ");
+//step 2:
+var titleCaseName = "";
+//step 3:
+for (var i = 0; i < words.length; i++) {
+    titleCaseName += words[i].charAt(0).toUpperCase() + words[i].slice(1).toLowerCase() + " ";
+}
+console.log(titleCaseName);
--- question3.ts
+++ question3.ts
@@ -1,7 +1,23 @@
 //Name Cases: Store a person’s name in a variable, and then print that person’s name in lowercase, uppercase, and titlecase.
 
-let personName1 = "rameez ahmed";
+let personName1 = "My name is Rameez ahmed khan";
 
+
+//LOWER CASE
 console.log(personName1.toLowerCase());
+
+//UPPER CASE
 console.log(personName1.toUpperCase());
-console.log(personName1.charAt(0).toUpperCase() + personName1.slice(1).toLowerCase());
+
+//TITLE CASE
+
+//step 1:
+let words: string[] = personName1.split(" ");
+//step 2:
+let titleCaseName:string = ""
+//step 3:
+for(let i=0; i<words.length; i++ ){
+    titleCaseName +=words[i].charAt(0).toUpperCase() + words[i].slice(1).toLowerCase() + " "
+}
+
+console.log(titleCaseName);
